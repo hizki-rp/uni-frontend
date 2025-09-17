44b43c8b9b7f3cdb24ef91852a56113ab7b6e5a8
hizki-rp <hibanim247@gmail.com>
2025-09-17 04:29:23 -0700

    remote login fix

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://uni-api-w0ms.onrender.com/";
+  import.meta.env.VITE_API_BASE_URL || "https://uni-api-w0ms.onrender.com";
